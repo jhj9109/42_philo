6f18379dd31ed93d4870d30f806ace26f555303b
jhj9109 <jhj91_09@naver.com>
2022-06-02 02:04:25 +0900

    Hotfix

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: hyeonjan <hyeonjan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 20:26:21 by hyeonjan          #+#    #+#             */
-/*   Updated: 2022/06/01 20:36:14 by hyeonjan         ###   ########.fr       */
+/*   Updated: 2022/06/02 02:03:38 by hyeonjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	parse(t_args **x_ptr, int ac, char **av)
 	t_args	*x;
 
 	x = *x_ptr;
-	if (!ft_atoi(av[1], &(x->number_philo) || \
-		!ft_atoi(av[2], &(x->time_die) || \
-		!ft_atoi(av[3], &(x->time_eat) || \
-		!ft_atoi(av[4], &(x->time_sleep) || \
+	if (!ft_atoi(av[1], &(x->number_philo)) || \
+		!ft_atoi(av[2], &(x->time_die)) || \
+		!ft_atoi(av[3], &(x->time_eat)) || \
+		!ft_atoi(av[4], &(x->time_sleep)) || \
 		(ac == 6 && !ft_atoi(av[5], &(x->number_goal_eat))))
 		exit_invalid(x_ptr, "Error\n", "invalid argument!\n");
 }

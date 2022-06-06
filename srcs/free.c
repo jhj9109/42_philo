47438dcf04f02e0d0f5730ef0a151bea295dc7df
jhj9109 <jhj91_09@naver.com>
2022-06-06 16:26:46 +0900

    Update free.c |add code for destroy mutex|
    뮤택스 거둬들이는 코드 추가
    - 종말단계이므로, 따로 실행결과를 따로 받아서 사용하지 않음

--- srcs/free.c
+++ srcs/free.c
@@ -6,11 +6,26 @@
 /*   By: hyeonjan <hyeonjan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 19:14:12 by hyeonjan          #+#    #+#             */
-/*   Updated: 2022/06/05 18:57:31 by hyeonjan         ###   ########.fr       */
+/*   Updated: 2022/06/06 16:24:42 by hyeonjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static void	_unset_mutex(t_args *x)
+{
+	int	i;
+
+	if (x->forks)
+	{
+		i = -1;
+		while (++i < x->number_philo)
+			pthread_mutex_destroy(&x->forks[i]);
+	}
+	pthread_mutex_destroy(&x->print);
+	pthread_mutex_destroy(&x->dead);
+	pthread_mutex_destroy(&x->end_mutex);
+}
 
 void	j_free(void **x)
 {
@@ -25,6 +40,7 @@ void	free_args(t_args *x)
 {
 	if (x)
 	{
+		_unset_mutex(x);
 		j_free((void **)&x->philos);
 		j_free((void **)&x->forks);
 	}
